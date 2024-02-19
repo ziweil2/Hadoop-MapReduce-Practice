67f81c5e197d99e14b09e6242db65b740cec57e1
Ziwei Liu <ziweivirgilliu@gmail.com>
2024-02-19 15:59:05 -0800

    changed part A sorting

--- PythonTemplate/TopTitlesReducer.py
+++ PythonTemplate/TopTitlesReducer.py
@@ -9,7 +9,7 @@ for line in sys.stdin:
     word, count = line.split('\t', 1)
     word_counts.append((word, int(count)))
 
-top_words = sorted(word_counts, key = lambda x: (-x[1],x[0]))
+top_words = sorted(word_counts, key = lambda x: (x[1],x[0]))
 
 for word, count in top_words:
     print('%s\t%s' % (word, count))
