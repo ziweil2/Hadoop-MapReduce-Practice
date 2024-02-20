a28c9264b2d03e09961f197c8d22db6b35013064
Ziwei Liu <ziweivirgilliu@gmail.com>
2024-02-19 22:10:38 -0800

    Fixed part B reducer

--- PythonTemplate/TopTitleStatisticsReducer.py
+++ PythonTemplate/TopTitleStatisticsReducer.py
@@ -8,15 +8,15 @@ for line in sys.stdin:
     _, count = line.strip().split('\t', 1)
     counts.append(int(count))
 
-_total = sum(counts)
-_mean = total / len(counts)
+total = sum(counts)
+mean = total / len(counts)
 _min = min(counts)
 _max = max(counts)
 
 variance = sum((x - mean) ** 2 for x in counts) / len(counts)
 
-print('%s\t%s' % ( 'Mean' , _mean))
-print('%s\t%s' % ( 'Sum' ,  _total))
+print('%s\t%s' % ( 'Mean' , mean))
+print('%s\t%s' % ( 'Sum' , total))
 print('%s\t%s' % ( 'Min' , _min))
 print('%s\t%s' % ( 'Max' , _max))
 print('%s\t%s' % ( 'Var' , variance))
